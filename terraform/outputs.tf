9a79fd8f8c07678e880bf8cca2850ab6b6202edd
Annie Christina <anniechristina027@gmail.com>
2025-04-02 18:21:45 +0530

    Removed Terraform folder from Git tracking

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "s3_bucket_name" {
-  value = aws_s3_bucket.pipeline_bucket.bucket
-}
-
-output "codepipeline_name" {
-  value = aws_codepipeline.my_pipeline.name
-}
-
--- terraform/s3.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "pipeline_bucket" {
-  bucket = var.s3_bucket_name
-}
-
